040f345516b59a966ec244d9215d7234ff828af9
dacet07 <140901107+dacet07@users.noreply.github.com>
2024-12-11 14:02:39 +0200

    Add files via upload

---
+++ ConsoleGame-with-Csharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The Number Guessing Game!");
+        Console.WriteLine("Number between 1 and 100.");
+
+        Random random = new Random();
+        int number = random.Next(1, 101);
+        int numberOfTries = 0;
+        int playerGuess = 0;
+
+        // Game loop
+        while (playerGuess != number)
+        {
+            Console.ResetColor();
+            Console.WriteLine("Enter your guess: ");
+            string? playerInput = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(playerInput))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+
+            // Validate input
+            if (!int.TryParse(playerInput, out playerGuess))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+
+            numberOfTries++;
+
+            if (playerGuess < number)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Too low!");
+            }
+            else if (playerGuess > number)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Congratulations! You've guessed the number in {numberOfTries} tries.");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ ConsoleGame-with-Csharp/README.md
@@ -0,0 +1,7 @@
+A simple console-based guessing game built with C#.
+
+- The game selects a random number between 1 and 100.
+- The player has to guess the number.
+- After each guess, the game will tell you if your guess is too high or too low.
+
+Made with C#
